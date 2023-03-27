9fd7cd4114eadb642dd3ad5045c8ea80ec4be335
som_nus <123231786+ssomnus@users.noreply.github.com>
2023-03-27 22:52:12 +0300

    Add files via upload

--- game/main.js
+++ game/main.js
@@ -1,12 +1,11 @@
-function register() {
+function signin() {
     const url = "https://sql.lavro.ru/call.php?";
-    let form = new FormData(document.getElementById("reg"));
     let fd = new FormData();
 
-    fd.append("pname", "Registration");
+    fd.append("pname", "SignIn");
     fd.append("db", "265117");
-    fd.append("p1", document.getElementById("log2").value);
-    fd.append("p2", document.getElementById("pas2").value);
+    fd.append("p1", document.getElementById("log1").value);
+    fd.append("p2", document.getElementById("pas1").value);
 
     fetch(url, {
         method: "POST",
@@ -23,15 +22,14 @@ function register() {
     });
 }
 
-function signin() {
+function register() {
     const url = "https://sql.lavro.ru/call.php?";
-    let form = new FormData(document.getElementById("sig"));
     let fd = new FormData();
 
-    fd.append("pname", "SignIn");
+    fd.append("pname", "Registration");
     fd.append("db", "265117");
-    fd.append("p1", document.getElementById("log1").value);
-    fd.append("p2", document.getElementById("pas1").value);
+    fd.append("p1", document.getElementById("log2").value);
+    fd.append("p2", document.getElementById("pas2").value);
 
     fetch(url, {
         method: "POST",
