746d6cd7b6b9cc3a382871b7770855d5b072c6f0
som_nus <123231786+ssomnus@users.noreply.github.com>
2023-03-28 13:00:47 +0300

    Add files via upload

--- game/main.js
+++ game/main.js
@@ -1,3 +1,12 @@
+const regButton = document.getElementById("regBut");
+const signButton = document.getElementById("signBut");
+const start = document.querySelector(".main");
+const roomList = document.querySelector(".rooms");
+const crRoomButton = document.querySelector(".cr-room");
+const countPlayer = document.querySelector(".cplayer-value");
+const moveTime = document.querySelector(".tmove-value");
+
+
 function signin() {
     const url = "https://sql.lavro.ru/call.php?";
     let fd = new FormData();
@@ -19,6 +28,11 @@ function signin() {
         }
     }).then((responseJSON) => {
         console.log(responseJSON)
+    });
+
+    signButton.addEventListener("click", () => {
+        start.classList.add("hide");
+        roomList.classList.remove("hide");
     });
 }
 
@@ -44,4 +58,33 @@ function register() {
     }).then((responseJSON) => {
         console.log(responseJSON)
     });
+
+    regButton.addEventListener("click", () => {
+        start.classList.add("hide");
+        roomList.classList.remove("hide");
+    });
+}
+
+function newRoom() {
+    const url = "https://sql.lavro.ru/call.php?";
+    fd.append("pname", "CreateRoom");
+    fd.append("db", "265117");
+    fd.append("p1", document.getElementById("count-player").value);
+    fd.append("p2", document.getElementById("time-move").value);
+
+    fetch(url, {
+        method: "POST",
+        body: fd
+    }).then((response) => {
+        if(response.ok){
+            return response.json()
+        }
+        else{
+            return error("Ошибка");
+        }
+    }).then((responseJSON) => {
+        console.log(responseJSON)
+    });
+
+    
 }
